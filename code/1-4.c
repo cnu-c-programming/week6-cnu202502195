946f466159f8fd10de582a5fe902ac4202411a2b
cnu202502195 <202502195@o.cnu.ac.kr>
2026-04-10 09:21:47 +0900

    1-4.c

--- code/1-4.c
+++ code/1-4.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
-void print_arr(int* p, int size) {
-    printf("%zu\n", sizeof(p));
-    for (int i=0; i<size; i++) {
-        printf("%d ", *(p+i));
-    }
-    printf("\n");
-}
-
 int main() {
-    int arr[5] = {1, 2, 3, 4, 5};
+    int arr3[2][3][4] = { 0 };
 
-    printf("%zu\n", sizeof(arr));
-    for (int i=0; i<5; i++)
-        printf("%d ", *(arr+i));
-    printf("\n");
-    
-    print_arr(arr, 5);
+    int (*p)[3][4] = arr3;
+
+    printf("%zu\n", sizeof(p));
+    printf("%zu\n", sizeof(*p));
+    printf("%zu\n", sizeof(**p));
+    printf("%zu\n", sizeof(***p));
+
+    printf("%p\n", (void*)p);
+    printf("%p\n", (void*)*p);
+    printf("%p\n", (void*)**p);
+    printf("%d\n", ***p);
 }
